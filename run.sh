3c73e1b438e85bca10e1a8af7a5991c8f200e244
Taaha Mohsin <taaha.b.mohsin@vanderbilt.edu>
2025-12-02 14:56:20 -0500

    Fixed typo

--- run.sh
+++ run.sh
@@ -16,4 +16,4 @@
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar --config ./configs --since 09/01/2025 --until 12/15/2025 --formats java md fxml sh bat gradle txt --timezone UTC+08
+java -jar RepoSense.jar --config ./config --since 09/01/2025 --until 12/15/2025 --formats java md fxml sh bat gradle txt --timezone UTC+08
